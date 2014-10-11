bae1a99d273330bc658ae38dfc1fb9bd02889444
Peter Lorenz <peters_kotlett@hotmail.com>
2014-10-11 19:40:01 +0200

    Update bubble_sort.c

--- bubble_sort.c
+++ bubble_sort.c
@@ -1,25 +1,36 @@
 #include <stdio.h>
 
-int rry[] = {5,2,3,6,4,1};
-int len = 6;
 
 
-void bubble();
-void check();
 
-int main()
+//--------------------------------------------------------
+//Function bubble()
+void bubble(int rry[], int size)
 {
-  printf("bubble:   ");
-  bubble(rry);
-  check(rry);
-
-  return 0;
+  int temp;
+  int i;
+  for (i = 0; i < size - 1; i++)
+  {
+    int j;
+    for (j = i + 1; j < size; j++)
+    {
+      if (rry[i] > rry[j])
+      {
+        temp = rry[j];
+        rry[j] = rry[i];
+        rry[i] = temp;
+      }
+    }
+  }
 }
 
-void check()
+//--------------------------------------------------------
+//Function check()
+//prints the test result
+void check(int rry[], int size)
 {
   int ok = 1;
-  for (int i = 0; i < len; i++)
+  for (int i = 0; i < size; i++)
   {
     printf("%d", rry[i]);
 
@@ -33,21 +44,14 @@ void check()
     printf(" FAIL\n");
 }
 
-void bubble()
+int main()
 {
-  int temp;
-  int i;
-  for(i = 0; i < len - 1; i++)
-  {
-    int j;
-    for(j = i + 1; j < len; j++)
-    {
-      if(rry[i] > rry[j])
-      {
-        temp = rry[j];
-        rry[j] = rry[i];
-        rry[i] = temp;
-      }
-    }
-  }
+  int A[] = { 5, 2, 3, 6, 4, 1 };                              // also srand possible for random numbers
+  int len = 6;
+  printf("bubble:   ");
+  bubble(A, len);
+  check(A, len);
+
+  getchar();
+  return 0;
 }
